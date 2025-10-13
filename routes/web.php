7092a58885cefe41cedd1819ff9cb7b87a200829
expertratul <seoratrul24@gmail.com>
2025-10-13 22:38:05 +0600

    route update line 10, 11, 12

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/heloocoder', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
 
+// Other route definitions...
+// For example:
+// Route::get('/posts', [PostController::class, 'index'])->name('posts
