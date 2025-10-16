16197685798db5bced3dc2bdbd0be624c13afef0
Mahamud Hasan <rabbitdniro@rabbit.local>
2025-10-16 19:02:01 +0600

    Updated Frontend: layouts, homepage

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Title Comes from Different Pages -->
+<title> @yield("title") </title>
+
+<!-- Custom CSS -->
+<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends("layouts.master")
+
+@section("title", "Home Page")
+
+@section("content")
+    <div class="main-container">
+        <h1>Welcome to FoodieLand</h1>
+        <p>Your ultimate destination for delicious recipes and food inspiration!</p>
+    </div>
+@endsection
