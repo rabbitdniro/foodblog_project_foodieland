59096926d953feb0a9c49ac883e20bde43c4437f
expertratul <seoratrul24@gmail.com>
2025-10-19 00:01:51 +0600

    Implemented Blog, Category, and User controllers; added CheckTokenExpires middleware; updated routes and composer lock file

---
+++ app/Http/Middleware/CheckTokenExpires.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckTokenExpires
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {   $token = $request->user()->currentAccessToken();
+
+        // No token at all
+        if (! $token) {
+            return response()->json([
+                'message' => 'Token missing or invalid',
+                'status'  => 401,
+            ], 401);
+        }
+        
+        // Check if token is expired 
+        if($token && $token->expires_at && $token->expires_at->isPast()) {
+            return response()->json([
+                'message' => 'Token has expired',
+                'status' => 401
+            
+            ], 401);
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-// Other route definitions...
-// For example:
-// Route::get('/posts', [PostController::class, 'index'])->name('posts
