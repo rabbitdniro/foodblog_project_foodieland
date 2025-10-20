ce61bbcb6b8b8795a8f2f955848107cf3d5611cb
Mahamud Hasan <rabbitdniro@rabbit.local>
2025-10-20 20:46:19 +0600

    added sql database

---
+++ resources/views/pages/blogs.blade.php
@@ -0,0 +1,10 @@
+@extends("layouts.master")
+
+@section("content")
+
+    <div class="bolg-container">
+        <h1>FoodieLand Blogs</h1>
+        <p>Discover our latest articles, recipes, and food tips!</p>
+    </div>
+
+@endsection
