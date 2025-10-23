e90b70d228bc27e70ad9544d9c4f461c40a1fed2
ehsan-mad <saadkhan420000@gmail.com>
2025-10-24 02:35:09 +0600

    blog& contact page ui done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,11 +27,11 @@
     </div>
 	  @include('components.header')
     <!-- END nav -->
-    
+
    @yield('content')
-		
+
     @include('components.footer')
-  
+
 
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
@@ -54,6 +54,6 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="assets/js/google-map.js"></script>
   <script src="assets/js/main.js"></script>
-    
+
   </body>
 </html>
